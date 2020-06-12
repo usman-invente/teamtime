4296b9c2dcc6dadbd168b6ce63249d15aeb7a2c4
Muhammaad Usmnan <usman.traximtech@gmail.com>
2020-06-12 21:32:42 +0500

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,153 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <div class="top-header">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <ul class="user-grid">
+                <li><i class="fa fa-user"></i></li>
+                <li><i class="fa fa-plus"></i></li>
+              </ul>
+            </div>
+            <div class="col-md-auto">
+              <div class="search-grid">
+                <input type="text" placeholder="Search teams, players, playgrounds"/>
+              </div>
+            </div>
+            <div class="col col-lg-2" style="text-align: right;">
+              <img src="@/assets/images/Logo.png"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 content-detail-section">
+            <div class="content-detail">
+              <div class="row">
+                <div class="col-sm-6">
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                </div>
+                <div class="col-sm-6 content-upload-section">
+                  <p><i class="fa fa-lock" aria-hidden="true"></i> TeamName 1</p>
+                </div>
+              </div>
+              <hr/>
+              <div class="row">
+                <div class="col-sm-6">
+                  <button class="more-detail">More Details</button>
+                </div>
+                <div class="col-sm-6">
+                  <button class="players">players 17 <i class="fa fa-users" aria-hidden="true"></i></button>
+                </div>
+                <img class="foot-ball" src="@/assets/images/foot-ball.png"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 content-upload-section">
+            <div class="content-upload">
+              <div class="row">
+                <div class="col-sm-8">
+                  <ul>
+                    <li>Herzeliya <i class="fa fa-map-marker" aria-hidden="true"></i></li>
+                    <li>Male <i class="fa fa-male" aria-hidden="true"></i></li>
+                    <li>24</li>
+                    <li>Sagi Hatan</li>
+                  </ul>
+                  <hr/>
+                </div>
+                <div class="col-sm-8">
+                  <p> Hey, I’m Sagi and I love football & swimming, Easy going guy who always up for sport! Hope to meet nice people!</p>
+                </div>
+              </div>
+              <div class="images-forms">
+                <form method="post" id="image-form">
+                  <input type="file" name="img[]" class="file" accept="image/*">
+                  <div class="input-group-append">
+                    <i class="fa fa-plus browse"></i>
+                  </div>
+                </form>
+                <div class="border-image">
+                  <img src="@/assets/images/Rectangle 2.png" id="preview" class="img-thumbnail">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 content-detail-sec-section">
+            <div class="content-detail mt-0">
+              <div class="row">
+                <div class="col-sm-6">
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                </div>
+                <div class="col-sm-6">
+                  <p><i class="fa fa-lock" aria-hidden="true"></i> TeamName 1</p>
+                </div>
+              </div>
+              <hr/>
+              <div class="row">
+                <div class="col-sm-6">
+                  <button class="more-detail">More Details</button>
+                </div>
+                <div class="col-sm-6">
+                  <button class="players">players 17 <i class="fa fa-users" aria-hidden="true"></i></button>
+                </div>
+                <img class="foot-ball" src="@/assets/images/ball.png"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 image-gallery-section">
+            <div class="image-gallery ">
+              <p>Photo</p>
+              <div class="row">
+                <figure class="col-md-3">
+
+                  <img alt="picture" src="@/assets/images/Rectangle 2.png" class="img-fluid"/>
+
+                </figure>
+                <figure class="col-md-3">
+
+                  <img alt="picture" src="@/assets/images/Rectangle 2.1.png" class="img-fluid" />
+
+                </figure>
+                <figure class="col-md-3">
+
+                  <img alt="picture" src="@/assets/images/Rectangle 2.1 (1).png" class="img-fluid" />
+
+                </figure>
+                <figure class="col-md-3">
+
+                  <img alt="picture" src="@/assets/images/Rectangle 2.1 (2).png" class="img-fluid" />
+
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <i class="fa fa-paper-plane" aria-hidden="true"></i>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/style.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 Vue.config.productionTip = false
 
